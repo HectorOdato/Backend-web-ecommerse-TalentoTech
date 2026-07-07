f771616c312ba5b15081df1503f6f362d1e21294
HectorOdato <leoodato@gmail.com>
2026-07-07 00:45:23 -0300

    primer commit

---
+++ src/test/java/gestionproductos/GestionproductosApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionproductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
