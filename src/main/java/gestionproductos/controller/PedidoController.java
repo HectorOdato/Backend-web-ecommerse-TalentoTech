e39506166286e94e5d98fad0d941c8acc1d943e0
HectorOdato <leoodato@gmail.com>
2026-07-08 00:39:37 -0300

    Fix controller y services de Pedidos

---
+++ src/main/java/gestionproductos/controller/PedidoController.java
@@ -0,0 +1,28 @@
+package gestionproductos.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import gestionproductos.model.Pedido;
+import gestionproductos.service.PedidoService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping
+    public List<Pedido> listarPedidos() {
+        return pedidoService.listarPedidos();
+    }
+
+    @PostMapping
+    public Pedido crearPedido(@Valid @RequestBody Pedido pedido) {
+        return pedidoService.crearPedido(pedido);
+    }
+}
--- src/main/java/gestionproductos/service/PedidoService.java
+++ src/main/java/gestionproductos/service/PedidoService.java
@@ -1,9 +1,7 @@
 package gestionproductos.service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import gestionproductos.model.LineaPedido;
 import gestionproductos.model.Pedido;
 import gestionproductos.model.Producto;
@@ -17,8 +15,7 @@ public class PedidoService {
     private final ProductoRepository productoRepository;
 
     public PedidoService(PedidoRepository pedidoRepository,
-                         ProductoRepository productoRepository) {
-
+                        ProductoRepository productoRepository) {
         this.pedidoRepository = pedidoRepository;
         this.productoRepository = productoRepository;
     }
@@ -27,4 +24,23 @@ public class PedidoService {
         return pedidoRepository.findAll();
     }
 
+    public Pedido crearPedido(Pedido pedido) {
+        for (LineaPedido linea : pedido.getLineas()) {
+
+            Producto productoDB = productoRepository.findById(linea.getProducto().getId())
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado con ID: " + linea.getProducto().getId()));
+
+
+            if (productoDB.getStock() < linea.getCantidad()) {
+                throw new RuntimeException("Stock insuficiente para el producto: " + productoDB.getNombre());
+            }
+
+            productoDB.setStock(productoDB.getStock() - linea.getCantidad());
+            productoRepository.save(productoDB);
+
+            linea.setProducto(productoDB);
+        }
+
+        return pedidoRepository.save(pedido);
+    }
 }
