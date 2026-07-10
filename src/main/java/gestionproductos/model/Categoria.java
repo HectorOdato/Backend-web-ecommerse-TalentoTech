d2a45097d25d8a4e3f0d8a18d9b0e704869abf59
HectorOdato <leoodato@gmail.com>
2026-07-10 09:46:52 -0300

    feat cree modelo categorias

---
+++ src/main/java/gestionproductos/model/Categoria.java
@@ -0,0 +1,48 @@
+package gestionproductos.model;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Document(collection = "categorias")
+public class Categoria {
+    @Id
+    private String id;
+
+    @NotBlank(message = "El nombre de la categoría no puede estar vacío.")
+    private String nombre;
+
+    private String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
